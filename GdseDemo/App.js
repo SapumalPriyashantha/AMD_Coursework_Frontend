1ab48f03a1352b750388ad1344b4756284c88875
SapumalPriyashantha <sapumalpriyashantha2000@gmail.com>
2022-10-07 23:37:28 +0530

    content updated

--- GdseDemo/App.js
+++ GdseDemo/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import SearchVehical from "./screens/SearchVehical";
 import SignIn from "./screens/SignIn";
 
 function App() {
   return (
-      <SignIn/>
+      <SearchVehical/>
   );
 }
 export default App;
